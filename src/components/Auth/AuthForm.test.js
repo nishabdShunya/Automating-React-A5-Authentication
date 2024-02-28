72a31e52145a9fc057a61fc6107e92f7914d6dfb
Nishant Choubisa <nishantchoubisa92@gmail.com>
2024-02-28 17:56:04 +0530

    Add: A5-T17-Implementing Login - Without Solution

--- src/components/Auth/AuthForm.test.js
+++ src/components/Auth/AuthForm.test.js
@@ -1,6 +1,6 @@
 import fs from "fs";
 
-test("Signup form submission correctly makes a post request with user credentials to the database", () => {
+test("Login form submission correctly makes a post request with user credentials to the database", () => {
   const fileContent = fs.readFileSync(
     "./src/components/Auth/AuthForm.js",
     "utf8"
@@ -9,11 +9,16 @@ test("Signup form submission correctly makes a post request with user credential
   // 1. Check for "JSON.stringify" at least once
   expect(fileContent).toMatch(/JSON\.stringify/g);
 
-  // 2. Check for correct url path
+  // 2. Check for correct url path for Sign Up
   expect(fileContent).toMatch(
     /https:\/\/identitytoolkit\.googleapis\.com\/v1\/accounts:signUp\?key=/
   );
 
-  // 3. Check for "POST" at least once
+  // 3. Check for correct url path for Login
+  expect(fileContent).toMatch(
+    /https:\/\/identitytoolkit\.googleapis\.com\/v1\/accounts:signInWithPassword\?key=/
+  );
+
+  // 4. Check for "POST" at least once
   expect(fileContent).toMatch(/POST/g);
 });
