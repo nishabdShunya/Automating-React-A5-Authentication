75317a1de359fec8c370a41173eec2262cb97558
Nishant Choubisa <nishantchoubisa92@gmail.com>
2024-03-06 00:14:30 +0530

    Add: A5-T21-Addressing Security & Logout Issues - With Solution

--- src/store/AuthContextProvider.js
+++ src/store/AuthContextProvider.js
@@ -2,16 +2,19 @@ import React, { useState } from "react";
 import AuthContext from "./auth-context";
 
 const AuthContextProvider = (props) => {
-  const [token, setToken] = useState(null);
+  const initialToken = localStorage.getItem("token");
+  const [token, setToken] = useState(initialToken);
 
   const isLoggedIn = !!token;
 
   const loginHandler = (token) => {
     setToken(token);
+    localStorage.setItem("token", token);
   };
 
   const logoutHandler = () => {
     setToken(null);
+    localStorage.removeItem("token");
   };
 
   const contextObject = {
